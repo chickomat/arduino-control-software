03b6eb4ba1c45529c74c75d251391322f040f200
Jonathan Treffler <mail@jonathan-treffler.de>
2020-07-25 15:24:08 +0200

    moved code from main repository

---
+++ Futtermaschine_Arduino/Futtermaschine_Arduino.ino
@@ -0,0 +1,59 @@
+#include "Ultrasonic.h"
+int incomingByte = 0;   
+int A =3;
+int in1 = A1;
+
+Ultrasonic ultrasonic(9,13);
+
+void setup() {
+  
+  Serial.begin(250000);   
+
+  
+  pinMode(12,OUTPUT);
+  pinMode(A,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  digitalWrite(5,HIGH);
+}
+void loop(){
+ incomingByte=0;
+
+ Serial.println(ultrasonic.Ranging(CM));
+ delay(1);
+  
+  
+  
+  }
+
+void serialEvent() {
+ if (Serial.available() > 0) {
+ 
+                //Serial.flush();
+                incomingByte = Serial.read();
+                
+                delay(1);
+                if(incomingByte = 65){
+               incomingByte=0;
+                digitalWrite(A,HIGH);
+                
+                delay(10000); //die Hälfte
+                
+                digitalWrite(A,LOW);
+                
+                
+                
+                incomingByte=0;
+                
+               
+                }
+                incomingByte=0;
+
+            
+                
+                }
+}
+
+
+
